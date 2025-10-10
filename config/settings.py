02623721dc42ae21998dd780953dda0834a6218a
Evgeniya Avksenteva <evgesh-092009@yandex.ru>
2025-10-10 22:00:06 +0300

    diary

--- config/settings.py
+++ config/settings.py
@@ -100,12 +100,17 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.CustomUser"
 
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/diary/'
+
+
 # EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_HOST = "smtp.yandex.ru"
-# EMAIL_PORT = 465
-# EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "True") == "True"
-# EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "False") == "True"
-# EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-# EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-# SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = "smtp.yandex.ru"
+EMAIL_PORT = 465
+EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "True") == "True"
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "False") == "True"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path  # добавляем include
+from django.shortcuts import redirect
+
+def root_redirect(request): return redirect('/accounts/login/')
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", root_redirect),
     path("diary/", include("diary.urls", namespace="diary")),
     path("users/", include("users.urls", namespace="users")),
     path("accounts/", include("django.contrib.auth.urls")),
